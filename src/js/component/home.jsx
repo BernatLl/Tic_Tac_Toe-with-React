b5f677c29f21e36265add6b8dfdf6d5dbf46fcec
BernatLl <blloret@hotmail.com>
2022-02-02 17:21:28 +0000

    www

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,27 +11,24 @@ const Home = () => {
 	const [turn, setTurn] = useState("x");
 
 	const CheckColumn = () => {
-		let i = 0;
+		let j = 0;
 		let ctrl = true;
 
-		while (ctrl && i < table.length) {
-			console.log(`i value: ${i}`);
-			// recorrer columnas => buscar NO coincidencias
-			let j = 1;
+		while (ctrl && j < table.length) {
+			let i = 1;
 			let ctrl2 = true;
-			while (ctrl2 && j < table[i].length) {
+			while (ctrl2 && i < table[j].length) {
 				if (
-					table[i][j] !== table[i][j - 1] ||
-					typeof table[i][j] === "undefined"
+					table[i][j] != table[i - 1][j] ||
+					typeof table[i][j] == "undefined"
 				) {
 					ctrl2 = false;
 				} else {
-					j += 1;
+					i += 1;
 				}
 			}
-
 			ctrl = !ctrl2;
-			i += 1;
+			j += 1;
 		}
 
 		return !ctrl;
@@ -39,47 +36,71 @@ const Home = () => {
 
 	const CheckMayorDiagonal = () => {
 		let i = 1;
+		let j = 1;
 		let ctrl = true;
 
-		while (ctrl && i < table.length) {
-			if (table[i][i] != table[i - 1][i - 1]) {
+		while (ctrl && i < table.length && j < table.length) {
+			if (
+				table[i][j] != table[i - 1][j - 1] ||
+				typeof table[i][j] === "undefined"
+			) {
 				ctrl = false;
 			} else {
 				i += 1;
+				j += 1;
 			}
 		}
 		return ctrl;
 	};
+	const CheckSecondDiagonal = () => {
+		let i = 1;
+		let j = 1;
+		let ctrl = true;
 
+		while (ctrl && i < table.length && j < table.length) {
+			if (
+				table[i][j] != table[i - 1][j + 1] ||
+				typeof table[i][j] === "undefined"
+			) {
+				crtl = false;
+			} else {
+				i += 1;
+				j -= 1;
+			}
+		}
+		return ctrl;
+	};
 	const CheckRows = () => {
-		// comprobar las filas
-		let ctrl = false;
 		let i = 0;
+		let ctrl = true;
 
-		while (ctrl && i < table.length) {
-			// * Los valores de los elementos
-			console.log(table[i]); // => FILA
-			let ctrl2 = false;
+		while (ctrl && j < table.length) {
 			let j = 1;
-
+			let ctrl2 = true;
 			while (ctrl2 && j < table[i].length) {
-				if (table[i][j - 1] != table[i][j]) {
-					ctrl2 = true;
+				if (
+					table[i][j] != table[i][j - 1] ||
+					typeof table[i][j] == "undefined"
+				) {
+					ctrl2 = false;
 				} else {
 					j += 1;
 				}
 			}
-
-			ctrl = ctrl2 ? true : false;
-			if (!ctrl) {
-				i += 1;
-			}
+			ctrl = !ctrl2;
+			i += 1;
 		}
-		return ctrl;
+
+		return !ctrl;
 	};
 
 	const CheckWinner = () => {
-		if (CheckRows() || CheckMayorDiagonal() || CheckColumn()) {
+		if (
+			CheckRows() ||
+			CheckMayorDiagonal() ||
+			CheckColumn() ||
+			CheckSecondDiagonal()
+		) {
 			alert("Hay un ganador");
 		}
 	};
@@ -100,21 +121,19 @@ const Home = () => {
 
 	return (
 		<>
-			<div className="container">
-				<table>
-					<tbody>
-						{table.map((row, i) => (
-							<tr key={i}>
-								{row.map((column, j) => (
-									<td onClick={() => SetValue(i, j)} key={j}>
-										<div className="item">{column}</div>
-									</td>
-								))}
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
+			<table>
+				<tbody>
+					{table.map((row, i) => (
+						<tr key={i}>
+							{row.map((column, j) => (
+								<td onClick={() => SetValue(i, j)} key={j}>
+									<div className="item">{column}</div>
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	);
 };
