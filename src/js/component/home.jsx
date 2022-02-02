d33049fe072a8530e4fa14a6f843a2f5c1fc5bda
BernatLl <blloret@hotmail.com>
2022-02-02 15:15:36 +0000

    www

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "./home.css";
 
@@ -11,28 +10,20 @@ const Home = () => {
 	]);
 	const [turn, setTurn] = useState("x");
 
-	useEffect(() => {
-		console.log(`Se está cargando mi componente`);
-	}, []);
-
-
-	// * TODO: Comprobar si alguien gana
-	/**
- * 1 - Comprobar las filas
- * 2 - Comprobar las columnas
- * 3 - Comprobar las diagonales
- */
 	const CheckColumn = () => {
 		let i = 0;
 		let ctrl = true;
 
-		while (ctrl && i < matrix.length) {
-			console.log(`i value: ${i}`)
+		while (ctrl && i < table.length) {
+			console.log(`i value: ${i}`);
 			// recorrer columnas => buscar NO coincidencias
 			let j = 1;
 			let ctrl2 = true;
-			while (ctrl2 && j < matrix[i].length) {
-				if ((matrix[i][j] !== matrix[i][j - 1]) || typeof matrix[i][j] === 'undefined') {
+			while (ctrl2 && j < table[i].length) {
+				if (
+					table[i][j] !== table[i][j - 1] ||
+					typeof table[i][j] === "undefined"
+				) {
 					ctrl2 = false;
 				} else {
 					j += 1;
@@ -41,26 +32,24 @@ const Home = () => {
 
 			ctrl = !ctrl2;
 			i += 1;
-
 		}
 
 		return !ctrl;
-
 	};
 
 	const CheckMayorDiagonal = () => {
 		let i = 1;
-		let ctrl = false;
+		let ctrl = true;
 
 		while (ctrl && i < table.length) {
 			if (table[i][i] != table[i - 1][i - 1]) {
-				ctrl = true;
+				ctrl = false;
 			} else {
 				i += 1;
 			}
 		}
 		return ctrl;
-	}
+	};
 
 	const CheckRows = () => {
 		// comprobar las filas
@@ -68,39 +57,32 @@ const Home = () => {
 		let i = 0;
 
 		while (ctrl && i < table.length) {
-
 			// * Los valores de los elementos
 			console.log(table[i]); // => FILA
 			let ctrl2 = false;
 			let j = 1;
 
 			while (ctrl2 && j < table[i].length) {
-
 				if (table[i][j - 1] != table[i][j]) {
 					ctrl2 = true;
 				} else {
 					j += 1;
 				}
-
 			}
 
 			ctrl = ctrl2 ? true : false;
 			if (!ctrl) {
 				i += 1;
 			}
-
 		}
 		return ctrl;
-	}
-
-
+	};
 
 	const CheckWinner = () => {
-		if (CheckRows() && CheckMayorDiagonal() && CheckColumn()) {
+		if (CheckRows() || CheckMayorDiagonal() || CheckColumn()) {
 			alert("Hay un ganador");
 		}
 	};
-
 
 	const ChangeTurn = () => setTurn(turn === "x" ? "o" : "x");
 
@@ -118,19 +100,21 @@ const Home = () => {
 
 	return (
 		<>
-			<table>
-				<tbody>
-					{table.map((row, i) => (
-						<tr key={i}>
-							{row.map((column, j) => (
-								<td onClick={() => SetValue(i, j)} key={j}>
-									<div className="item">{column}</div>
-								</td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<div className="container">
+				<table>
+					<tbody>
+						{table.map((row, i) => (
+							<tr key={i}>
+								{row.map((column, j) => (
+									<td onClick={() => SetValue(i, j)} key={j}>
+										<div className="item">{column}</div>
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</>
 	);
 };
